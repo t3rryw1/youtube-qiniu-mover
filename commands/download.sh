6bf040f04a10ea8dac9ae31dc773da1b4010ec3f
zhichangterry <zhichangterry@gmail.com>
2017-07-17 14:53:41 -0700

    init

---
+++ commands/download.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+tmp=tmp
+
+sort $1 -u > "tmp/$(basename $1).tmp"
+sort $2 -u > "tmp/$(basename $2).tmp"
+comm  -23 "tmp/$(basename $1).tmp" "tmp/$(basename $2).tmp" > "tmp/current_files.tmp"
+
+printf "There are $(wc -l < tmp/current_files.tmp) new files to be downloaded\n";
+while read url; do
+  printf "Start downloading $url\n"
+#  file_name=$(youtube-dl --get-filename -o 'videos/%(title)s.%(ext)s' $url)
+#  echo $file_name
+  youtube-dl -o "videos/%(title)s.%(ext)s" $url
+  printf "Finish downloading $url\n"
+  printf "Calling qshell to upload file"
+  qshell qupload
+
+  printf "Write $url to done List\n"
+  printf "$url\n" >> $2
+done < tmp/current_files.tmp
+
---
+++ go.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+git pull
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    qshell=./commands/qshell-linux-x64
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    qshell=./commands/qshell-darwin-x64                # Mac OSX
+fi
+
+eval $(cat ./.env | xargs)
+
+$qshell account $ACCESS_KEY $SECRET_KEY
+
+file1=${1:-'to-do-list/to-do-list.txt'}
+file2=${2:-'done-list/done-list.txt'}
+printf "Start processing file list...
+[$file1] \tas video file list,
+[$file2] \tas the finished file list\n"
+
+./commands/download.sh $file1 $file2
+
+$qshell qupload ./qshell_config.json
