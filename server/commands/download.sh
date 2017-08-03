893c899997a5cf3df0cd8b7f598af3092a1bde62
zhichangterry <zhichangterry@gmail.com>
2017-08-03 13:49:58 -0700

    video format

--- server/commands/download.sh
+++ server/commands/download.sh
@@ -10,7 +10,7 @@ while read url; do
   printf "Start downloading $url\n"
 #  file_name=$(youtube-dl --get-filename -o 'videos/%(title)s.%(ext)s' $url)
 #  echo $file_name
- if /usr/local/bin/youtube-dl -o "videos/%(id)s-%(autonumber)s.%(ext)s" $url ; then
+ if /usr/local/bin/youtube-dl -o "videos/%(release_date)s/%(creator)s/%(id)s-%(autonumber)s.%(ext)s" $url ; then
    printf "Finish downloading $url\n"
    printf "Write $url to done List\n"
    printf "$url\n" >> $2
