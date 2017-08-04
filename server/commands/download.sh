df758637de161b6259d84939a2df8029917b25af
zhichangterry <zhichangterry@gmail.com>
2017-08-03 19:53:19 -0700

    fix

--- server/commands/download.sh
+++ server/commands/download.sh
@@ -5,6 +5,7 @@ sort $2 -u > "tmp/$(basename $2).tmp"
 comm  -23 "tmp/$(basename $1).tmp" "tmp/$(basename $2).tmp" > "tmp/current_files.tmp"
 
 printf "There are $(wc -l < tmp/current_files.tmp) new files to be downloaded\n";
+echo "" > tmp/current_local_files.tmp
 while read url; do
   printf "Start downloading $url\n"
  file_name=$(/usr/local/bin/youtube-dl --get-filename -o "videos/%(upload_date)s/%(uploader)s/%(title)s.%(ext)s" $url)
@@ -12,7 +13,7 @@ while read url; do
  if /usr/local/bin/youtube-dl -o "videos/%(upload_date)s/%(uploader)s/%(title)s.%(ext)s" $url ; then
    printf "Finish downloading $url\n"
    printf "Write $url to done List\n"
-   printf "$file_name\n" > "tmp/current_local_files.tmp"
+   printf "$file_name\n" >> "tmp/current_local_files.tmp"
    printf "$url\n" >> $2
  else
    printf "Error downloading $url\n"
