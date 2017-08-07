f9ef6da8cf8115f41e878569f7ddc6b14729dfb9
zhichangterry <zhichangterry@gmail.com>
2017-08-07 10:30:32 -0700

    add grab by playlist

---
+++ client/add_video_list.sh
@@ -0,0 +1,16 @@
+cd $(dirname $0)
+echo $(PWD)
+
+# import env file
+eval $(cat ../.env | xargs)
+
+if [ -z "$1" ]; then
+  echo "Usage: ./add_video_list.sh url"
+  exit;
+fi
+
+youtube-dl --flat-playlist $1 --print-json | sed -n "s/^.*\"id\": \"\([^\"]*\).*/\1/gp" >> ../file-list/to-do/to-do-list.txt
+
+# echo "Adding $1 to download list"
+# printf "$1\n" >>../file-list/to-do/to-do-list.txt
+sshpass -p $RSNYC_PASSWORD rsync -a ../file-list/to-do/ $RSYNC_SERVER_USER@$RSYNC_SERVER_ADDRESS:$RSYNC_SERVER_PATH/file-list/to-do
